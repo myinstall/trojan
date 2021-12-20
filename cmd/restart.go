d0a9659e203166e853b4b70f1410d15d2751581e
myinstall <96402022+myinstall@users.noreply.github.com>
2021-12-20 12:10:56 +0800

    Update restart.go

--- cmd/restart.go
+++ cmd/restart.go
@@ -8,7 +8,7 @@ import (
 // restartCmd represents the restart command
 var restartCmd = &cobra.Command{
 	Use:   "restart",
-	Short: "重启trojan",
+	Short: "重启程序",
 	Run: func(cmd *cobra.Command, args []string) {
 		trojan.Restart()
 	},
